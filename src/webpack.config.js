370784af4d9a8374ee2eeb1a2f2b828789cd1220
ElizPN <pankevych.liza@gmail.com>
2022-02-23 16:50:19 +0100

    import css file

---
+++ src/webpack.config.js
@@ -0,0 +1,15 @@
+{
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ];
+  }
+}
