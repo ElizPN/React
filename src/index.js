c1987a0f66fa133ec4549601bb78adf452260636
ElizPN <pankevych.liza@gmail.com>
2022-02-25 20:19:25 +0100

    last version

--- src/index.js
+++ src/index.js
@@ -2,26 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      squares: Array(9).fill(null),
     };
   }
 
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -46,6 +51,18 @@ class Board extends React.Component {
           {this.renderSquare(8)}
         </div>
       </div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
     );
   }
 }
